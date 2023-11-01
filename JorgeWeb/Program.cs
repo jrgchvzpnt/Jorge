7bcba69e48b3665292018285bd09e776eccbc69c
Jorge Ernesto Chavez Puente <jrgchvzpnt@gmail.com>
2023-11-01 12:15:16 -0700

    Se termina la pagina de Home y detalles de los libros

--- JorgeWeb/Program.cs
+++ JorgeWeb/Program.cs
@@ -2,6 +2,7 @@ using JorgeBook.DataAccess.Data;
 using JorgeBook.DataAccess.Repository;
 using JorgeBook.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 // agregar el repositorio por inyeccion de dependencias
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -29,6 +32,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
